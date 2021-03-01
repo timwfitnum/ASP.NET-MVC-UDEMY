735eb01c81de4b99be0b56dbc712d975ec476785
timwfitnum <72702393+timwfitnum@users.noreply.github.com>
2021-03-01 18:15:52 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dbStuff.Startup))]
+namespace dbStuff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
